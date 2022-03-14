51fe6c61d66c0422304fbde6a7c3ec9e552f38d5
Valter Silva <53379816+valterchess@users.noreply.github.com>
2022-03-14 17:18:28 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App style={{backgroundColor:"#192a56"}} />
   </React.StrictMode>,
   document.getElementById('root')
 );
