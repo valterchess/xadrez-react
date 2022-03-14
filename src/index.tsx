6b818e8a666869105ea369f587e9e7ce332e8089
Valter Silva <53379816+valterchess@users.noreply.github.com>
2022-03-14 17:26:28 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App style={{backgroundColor:"#1e272e"}} />
-  </React.StrictMode>,
+  </React.StrictMode>
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
